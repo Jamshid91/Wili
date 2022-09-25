444ca3b795623858c48af310e5a8e89cc61d8376
Jamshid91 <iymon0217@gmail.com>
2022-09-25 23:54:04 +0500

    added

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,13 +1,49 @@
 const mobileMenu = document.querySelector('.mobile-menu'),
         menuBurger = document.querySelector('.menu-burger'),
       mobileMenuLists = document.querySelectorAll('.mobile-menu ul li'),
-      body = document.querySelector('body');
+      body = document.querySelector('body'),
+      catalog = document.querySelector('.catalog'),
+      catalogMob = document.querySelector('.catalog-mob'),
+      catalogInfo = document.querySelector('.catalog-info'),
+      more = document.querySelector('.more'),
+      moreInfo = document.querySelector('.more-info'),
+      moreMob = document.querySelector('.more-mob'),
+      moreInfoMob = document.querySelector('.more-info-mob'),
+      selectionBtn = document.querySelectorAll('.selection__btn'),
+      selectionInfoList = document.querySelectorAll('.selection-info-list');
 
 menuBurger.addEventListener('click', () => {
     menuBurger.classList.toggle('change');
     mobileMenu.classList.toggle('showMobileMenu');
     body.classList.toggle('showScroll')
 });
+
+catalog.addEventListener('click', () => {
+  catalogInfo.classList.toggle('d-none')
+});
+catalogMob.addEventListener('click', () => {
+  catalogInfo.classList.toggle('d-none')
+});
+
+moreInfo.addEventListener('click', () => {
+  more.classList.toggle('d-none')
+});
+moreInfoMob.addEventListener('click', () => {
+  moreMob.classList.toggle('d-none')
+});
+
+selectionBtn.forEach(btn => {
+  btn.addEventListener('click', () => {
+    btn.nextElementSibling.classList.toggle('d-none')
+  });
+})
+
+selectionInfoList.forEach(elem => {
+  elem.addEventListener('click', () => {
+    elem.parentElement.parentElement.children[0].children[0].children[1].children[1].innerText = elem.innerText;
+    elem.parentElement.classList.toggle('d-none');
+  })
+})
 
 
 $('.wrapper-slider').slick({
